e550b1dd636113293f82f2c38778e3a632640e43
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-09-30 00:21:25 +0700

    unwrapped

--- src/mods/parse/index.ts
+++ src/mods/parse/index.ts
@@ -1,6 +1,7 @@
 import { Guard } from "mods/guard/index.js"
 import { RecordGuard, StrongEqualityGuard, TupleGuard } from "mods/guards/index.js"
 import { Property } from "mods/props/index.js"
+import { optional, readonly, string } from "mods/toolbox/index.js"
 
 export type Parsed<T> =
   T extends any[] ? never :
@@ -26,16 +27,24 @@ export function parse<T>(value: T): Parsed<T> {
     return new StrongEqualityGuard(value) as any
 
   if (Array.isArray(value))
-    return new TupleGuard(value.map(x => parse(x))) as any
+    return new TupleGuard(value.map(x => parse(x)) as any) as any
 
   if ("asOrThrow" in value)
     return value as any
 
-  return new RecordGuard(Object.fromEntries(Object.entries(value).map(([k, v]) => {
-    if (v instanceof Property.Readonly)
-      return [k, new Property.Readonly(parse(v.value))]
-    if (v instanceof Property.Optional)
-      return [k, new Property.Optional(parse(v.value))]
-    return [k, parse(v)]
-  }))) as any
+  function subparse<T>(value: T): Subparsed<T> {
+    if (value instanceof Property.Optional)
+      return new Property.Optional(subparse(value.value)) as any
+    if (value instanceof Property.Readonly)
+      return new Property.Readonly(subparse(value.value)) as any
+    return parse(value) as any
+  }
+
+  return new RecordGuard(Object.fromEntries(Object.entries(value).map(([k, v]) => [k, subparse(v)]))) as any
 }
+
+const x = parse({
+  a: optional(readonly(string())),
+})
+
+x.guard.a
